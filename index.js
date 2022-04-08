d8e0f0b727e687542296474362ed2a12749da89a
+ <+>
2022-04-08 14:14:33 -0400

    commit tree 2cb1afb6d32d8af81bf32054ff6f5a1ae1c8208b

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const main = document.querySelector('main');
+main.remove()
+
+let newHeader = document.querySelector('h1#victory')
+newHeader.textContent = 'Logan is the champion'
+
